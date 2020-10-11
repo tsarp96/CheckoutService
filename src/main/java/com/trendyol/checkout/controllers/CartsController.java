183081f0cf72e26ff65cf2eb91206a34028d9c2f
tsarp96 <tsarp96@hotmail.com>
2020-10-11 23:42:53 +0300

    CartController Implementation

--- src/main/java/com/trendyol/checkout/controllers/CartsController.java
+++ src/main/java/com/trendyol/checkout/controllers/CartsController.java
@@ -1,12 +1,15 @@
 package com.trendyol.checkout.controllers;
 
+import ch.qos.logback.core.pattern.util.RegularEscapeUtil;
 import com.trendyol.checkout.domain.Cart;
 import com.trendyol.checkout.services.CartsService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/carts")
@@ -19,8 +22,32 @@ public class CartsController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> addCart(@RequestBody Cart cart) {
+    public ResponseEntity<Void> createCart(@RequestBody Cart cart) {
         cartsService.createCart(cart);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(cart.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/{cartId}")
+    public ResponseEntity<Cart> deleteById(@PathVariable String  cartId){
+        cartsService.deleteById(cartId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cart>> getCartByUserId(@RequestParam(name = "userId") String userId){
+        List<Cart> carts = cartsService.getCartByUserId(userId);
+        return ResponseEntity.ok(carts);
+    }
+
+    @PostMapping("/{cartId}/items/{itemId}")
+    public ResponseEntity addItem(@PathVariable String cartId, @PathVariable String itemId){
+        cartsService.addItemToCart(cartId, itemId);
         return ResponseEntity.ok().build();
     }
+
 }
